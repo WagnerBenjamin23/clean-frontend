d30ea0c82f2e987318a038c61e0cf7f3cb3ae482
WagnerBenjamin23 <wagnerbenjamin23@gmail.com>
2026-01-30 09:32:08 -0300

    fix interceptor

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -6,20 +6,30 @@ import { environment } from 'src/environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+
   const token = localStorage.getItem('token');
+  const apiKey = environment.apiKeyBackend;
 
   const authReq = req.clone({
     setHeaders: {
-      Authorization: token ? `Bearer ${token}` : '',
-      'x-api-key': environment.apiKeyBackend
+      ...(token && { Authorization: `Bearer ${token}` }),
+      'x-api-key': apiKey
     }
   });
 
   return next(authReq).pipe(
     tap({
       error: (error) => {
-        if (error instanceof HttpErrorResponse && error.status === 401) {
-          router.navigateByUrl('/login');
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            console.warn('Token inválido o expirado, limpiando storage...');
+            localStorage.clear();
+            sessionStorage.clear();
+            router.navigateByUrl('/login');
+          }
+          if (error.status === 403) {
+            console.error('API_KEY inválida');
+          }
         }
       }
     })
